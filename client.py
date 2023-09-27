0622bbc51858097ca3c643ed9fe5a581931dba63
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2023-09-27 16:10:23 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,19 +1,11 @@
 import socket
 import threading
-from plyer import notification  # Import the notification module from plyer
+from plyer import notification
 
 # Connection Data
 host = 'chat.bunk.pro'
 port = 8080
-print(r""" #$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#
-   ____ _           ____ _   _    _  _____ 
-  / ___| |         / ___| | | |  / \|_   _|
- | |   | |   _____| |   | |_| | / _ \ | |  
- | |___| |__|_____| |___|  _  |/ ___ \| |  
-  \____|_____|     \____|_| |_/_/   \_\_|  
-            By Abhishek Dash
-#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#                                     
-     """)
+
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
@@ -21,55 +13,72 @@ client.connect((host, port))
 # Prompting The Client For A Nickname
 nickname = input("Choose your nickname: ")
 
+# Flag to track if connected notification has been sent
+connected_notification_sent = False
+
 # Listening To Server And Sending Nickname
 def receive():
+    global connected_notification_sent
     while True:
         try:
             # Receive Message From Server
             message = client.recv(1024).decode('ascii')
 
             if message == 'NICK':
-                # If 'NICK', Send Nickname
                 client.send(nickname.encode('ascii'))
-            
-            elif message.lower() == f'{nickname}: leave':
-                # Notify the user about leaving the chat
-                print("You left the chat room.")
-                client.close()
-                break
+                # Send connected notification only once
+                if not connected_notification_sent:
+                    connected_notification_sent = True
 
             else:
-                # Display the received message as a notification (except for own messages)
-                sender, _, _ = message.partition(':')
-                if sender.lower() != nickname.lower():
-                    notification.notify(
-                        title=f"New Message from {sender}",
-                        message=message,
-                        app_name="CL-Chat",  # You can customize this
-                        timeout=10,  # Notification will disappear after 10 seconds
-                    )
+                # Check if the message is "leave" to exit the chat
+                if message.lower() == f'{nickname}: leave':
+                    print("You left the chat room.")
+                    client.close()
+                    break
+                else:
+                    # Display the received message without timestamp
+                    sender, _, received_message = message.partition(':')
+                    # Exclude server-related messages
+                    if sender.lower() != "server" and received_message.lower().strip() != "get / http/1.1":
+                        print(f"{sender}: {received_message.strip()}")  # Print the message without timestamp
 
-                print(message)
+                        # Display notification for others' messages
+                        if sender.lower() != nickname.lower():
+                            # Check for join and exit messages
+                            if received_message.endswith("joined the chat.") or received_message.endswith("left the chat."):
+                                notification.notify(
+                                    title="CL-Chat Notification",
+                                    message=received_message,
+                                    app_name="CL-Chat",
+                                    timeout=10,
+                                )
+                            else:
+                                # Display notification for others' messages
+                                notification.notify(
+                                    title=f"New Message from {sender}",
+                                    message=received_message,
+                                    app_name="CL-Chat",
+                                    timeout=10,
+                                )
+
         except:
             # Close Connection When Error
-            print("Err...Okay...nvm..LoL")
+            print("An error occurred!")
             client.close()
             break
-
 
 # Sending Messages To Server
 def write():
     while True:
-        message = input('You: ')
+        message = input('')  # Empty string to prevent displaying the input prompt
         if message.lower() == 'leave':
-            client.send(message.encode('ascii'))
+            client.send(f'{nickname}: leave'.encode('ascii'))
             print("You left the chat room.")
             client.close()
             break
         else:
-            message = '{}: {}'.format(nickname, message)
-            client.send(message.encode('ascii'))
-            print('\033[F\033[K', end='')  # Clears the input line
+            client.send(f'{nickname}: {message}'.encode('ascii'))
 
 # Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
